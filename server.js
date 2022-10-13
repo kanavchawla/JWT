24f7677ce16091540f63b1040940250f0cf0eff5
kanavchawla <kchawla1_be21@thapar.edu>
2022-10-14 01:25:26 +0530

    code

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const posts = [
+    {
+        username:'jim',
+        title:'post1'
+    },
+    {
+        username:'tom',
+        title:'post2'
+    }
+]
+app.use(express.json());
+const jwt = require('jsonwebtoken');
+app.get('/posts',(req,res)=>{
+    res.json(posts);
+})
+app.post('/login', (req, res) => {
+    //authenticate user
+
+  const username = req.body.username;
+  const user= {name:username} 
+  jwt.sign(user,process.env.ACESS_TOKEN_SECRET)
+});
+app.listen(3000);
